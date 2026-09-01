95b3492d1717a15fbf134e374a6a83c37e53f6a7
me <me@example.com>
2026-09-01 16:11:44 +0200

    chore: scaffolded project structure using template: project-scripts-scaffold-template.txt

---
+++ scripts/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+# PURPOSE:
+# Start application locally via docker-compose
+
+echo "Start application placeholder - not implemented yet"
+# docker-compose up -d
+
+# Remove file if not needed.
